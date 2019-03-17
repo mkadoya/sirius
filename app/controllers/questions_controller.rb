94e59258143764eb4d6ff9bffaf366eadd8223f9
mkadoya <mkadoya@DESKTOP-MKID2K7.localdomain>
2019-03-17 18:05:31 +0900

    add Category column to Result db

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,7 @@ class QuestionsController < ApplicationController
       @question_ids = params[:question_ids]
       @answers.push(params[:answer])
     else
+      # 借りの値を1行入れる。のちに削除する。
       @answers = ["0"]
       @question_ids = ["0"]
     end
@@ -19,15 +20,20 @@ class QuestionsController < ApplicationController
       @question = @questions.last(@questions_num - @answers_count).first
       @question_ids.push(@question.question_id)
     else
+      # 借りの1行目を削除する
       @answers.shift(1)
       @question_ids.shift(1)
-      redirect_to :controller => "questions", :action => "result", :answers => @answers, :question_ids => @question_ids
+      # カテゴリーの値を入れる
+      @category = Question.find_by(question_id:@question_ids[0]).category
+      
+      redirect_to :controller => "questions", :action => "result", :answers => @answers, :question_ids => @question_ids, :category => @category
     end
   end
 
   def result
     @answers = params[:answers]
     @question_ids = params[:question_ids]
+    @category = params[:category]
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_053640) do
+ActiveRecord::Schema.define(version: 2019_03_17_083901) do
 
   create_table "characteristics", force: :cascade do |t|
     t.string "category"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_03_17_053640) do
     t.boolean "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
