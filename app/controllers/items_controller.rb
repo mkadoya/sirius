49cb0b939e913ac6153449a1d9fffb67a7bd9df5
mkadoya <mkadoya@DESKTOP-MKID2K7.localdomain>
2019-03-24 17:35:12 +0900

    change each color item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,26 @@
 class ItemsController < ApplicationController
   def show
-    @item = Item.find_by(item_id: params[:item_id])
+    # @item = Item.find_by(item_id: params[:item_id])
+    @series = params[:series]
+    @items = Item.where(series: @series).all
+
+    @items_array = [@items.first]
+    @items.each do |item|
+      @items_array.push(item)
+    end
+    @items_array.shift
+    @item = @items_array[0]
+  end
+  def series
+    @series = "MacBook Air"
+    @items = Item.where(series: @series).all
+
+    @items_array = [@items.first]
+    @items.each do |item|
+      @items_array.push(item)
+    end
+    @items_array.shift
+    @item = @items_array[0]
+
   end
 end
