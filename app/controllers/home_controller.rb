cb546a08929f280a8fe6a4ef69857da6d5dfa574
mkadoya <mkadoya@DESKTOP-MKID2K7.localdomain>
2019-05-03 21:05:56 +0900

    change Ideapad

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,7 +17,7 @@ class HomeController < ApplicationController
     @laptops.push(@laptop)
     @laptop = {"image" => Item.find_by(series:"VivoBook S13 S330UA").image, "url" => "10", "series" => "VivoBook S13 S330UA"}
     @laptops.push(@laptop)
-    @laptop = {"image" => Item.find_by(series:"Ideapad S130 ").image, "url" => "46", "series" => "Ideapad S130 "}
+    @laptop = {"image" => Item.find_by(series:"Ideapad S130").image, "url" => "46", "series" => "Ideapad S130 "}
     @laptops.push(@laptop)
     @laptop = {"image" => Item.find_by(series:"ZenBook 14 UX430UA").image, "url" => "16", "series" => "ZenBook 14 UX430UA"}
     @laptops.push(@laptop)
