d1aa49a216ae2001aa635476ab488b3acde9937d
mkadoya <mkadoya@DESKTOP-MKID2K7.localdomain>
2019-05-07 23:14:18 +0900

    remove OptionResult

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
   resources :images
-  get 'options/get' => "options#get"
   get 'items/show/:item_id' => "items#show"
   get 'results/index'
   get 'questions/index' => "questions#index"
