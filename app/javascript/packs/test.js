d31c8a6d8419de75fd05bfe5bc4ef7f6afc9cbd9
mkadoya <mkadoya@DESKTOP-MKID2K7.localdomain>
2019-06-22 08:02:48 +0900

    change Movies

--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -7,8 +7,17 @@ const app = new Vue({
     data: {
         movieInfo: {},
         checkedTags: [],
+        pushedTags: [],
         movieList: [],
         tagList: [],
+        isActive: true,
+    },
+    mounted() {
+        axios.get(`movie/movie_tag/0`)
+            .then(res => {
+                console.log(res.data);
+                this.tagList = res.data;
+            });
     },
     methods: {
         setMovieInfo(id) {
@@ -18,9 +27,35 @@ const app = new Vue({
                     this.movieInfo = res.data;
                 });
         },
+        setPushedTag(name) {
+            var flag = true;
+            for (let i = 0; i < this.pushedTags.length; i++) {
+                if (this.pushedTags[i] == name) {
+                    flag = false;
+                    this.pushedTags.splice(i, 1);
+                }
+            }
+            if (flag) {
+                this.pushedTags.push(name);
+            }
+        },
     },
     watch: {
         checkedTags: function (val) {
+            axios.get(`movie/tag/${val}`)
+                .then(res => {
+                    console.log(res.data);
+                    this.movieList = res.data;
+                });
+            if (val == '') {
+                axios.get(`movie/movie_tag/0`)
+                    .then(res => {
+                        console.log(res.data);
+                        this.tagList = res.data;
+                    });
+            }
+        },
+        pushedTags: function (val) {
             axios.get(`movie/tag/${val}`)
                 .then(res => {
                     console.log(res.data);
@@ -45,5 +80,6 @@ const app = new Vue({
                     this.tagList = res.data;
                 });
         },
-    }
+    },
+
  })
