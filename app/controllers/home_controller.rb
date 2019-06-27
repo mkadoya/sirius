7763d4e8a7c26eefe5c842cd58802bec7db8a0d1
mkadoya <mkadoya@DESKTOP-MKID2K7.localdomain>
2019-06-28 07:44:20 +0900

    change each tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -41,34 +41,105 @@ class HomeController < ApplicationController
     render json: @movie
   end
 
+  def tagToTag
+    all_tags = Tag.all
+    @tagArray = Array.new
+    if params[:tag] == "0"
+      all_tags.each do |tag|
+        tags = Array.new
+        tags_movie = Tag.where(name: tag.name).pluck(:movie_id).uniq
+        isFirst = true
+        tags_movie.each do |movie|
+          temp_tags = Tag.where(movie_id: movie).pluck(:name).uniq
+          if isFirst
+            tags = temp_tags
+            isFirst = false
+          end
+          temp_tags.each do |temp_tag|
+            unless tags.include?(temp_tag)
+              tags.push(temp_tag)
+            end
+          end
+        end
+        hash = {tag: tag.name, tags: tags}
+        @tagArray.push(hash)
+      end
+    else
+      tags_movie = Tag.where(name: params[:tag]).pluck(:movie_id).uniq
+      isFirst = true
+      tags = Array.new
+      tags_movie.each do |movie|
+        temp_tags = Tag.where(movie_id: movie).pluck(:name).uniq
+        if isFirst
+          tags = temp_tags
+          isFirst = false
+        end
+        temp_tags.each do |temp_tag|
+          unless tags.include?(temp_tag)
+            tags.push(temp_tag)
+          end
+        end
+      end
+      hash = {tag: params[:tag], tags: tags}
+      @tagArray.push(hash)
+    end
+    render json: @tagArray
+  end
+
   def tag
     @tag_names = params[:name].split(",")
     @movies = Array.new
     isFirst = true
     if params[:name] == "0"
-      @movies = Movie.all
+      tags = Tag.all.pluck(:name).uniq
+      all_movies = Movie.all
+      movies_list = Array.new
+      all_movies.each do |movie|
+        movie_hash = {id: movie.id, image: movie.image.url}
+        movies_list.push(movie_hash)
+      end
+      hash = {tag: "all", movies: movies_list }
+      @movies.push(hash)
+      tags.each do |tag|
+        tag_movies = Tag.where(name:tag).all
+        movies_list = []
+        tag_movies.each do |tag_movie|
+          movie_image = Movie.find_by(id:tag_movie.movie_id ).image.url
+          movie_hash = {id: tag_movie.movie_id, image: movie_image}
+          movies_list.push(movie_hash)
+        end
+        hash = {tag: tag, movies: movies_list }
+        @movies.push(hash)
+      end
     else
+      movies_list = Array.new
       @tag_names.each do |name|
         tags = Tag.where(name: name).all
         unless tags.nil?
           if isFirst
             tags.each do |tag|
               movie = Movie.find_by(id: tag.movie_id)
-              @movies.push(movie)
+              # @movies.push(movie)
+              movie_hash = {id: movie.id, image: movie.image.url}
+              movies_list.push(movie_hash)
             end
             isFirst = false
           else
             new_movies = Array.new
             tags.each do |tag|
               movie = Movie.find_by(id: tag.movie_id)
-              new_movies.push(movie)
+              # new_movies.push(movie)
+              movie_hash = {id: movie.id, image: movie.image.url}
+              new_movies.push(movie_hash)
             end
-            @movies = @movies & new_movies
+            # @movies = @movies & new_movies
+            movies_list = movies_list & new_movies
           end
         end
       end
+      hash = {tag: @tag_names, movies: movies_list }
+      @movies.push(hash)
     end
-
     render json: @movies
   end
 
@@ -103,6 +174,21 @@ class HomeController < ApplicationController
       end
     end
     render json: @tags
+  end
+
+
+
+  def checkTagAndMovie
+    @data = params[:data].split(",")
+    @result = false
+    if @data.length == 2
+      movie_id = @data[0]
+      tag = @data[1]
+      if Tag.where(movie_id: movie_id, name: tag).all.count > 0
+        @result = true
+      end
+    end
+    render json: @result
   end
 
 end
--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -9,6 +9,9 @@ const app = new Vue({
         pushedTags: [],
         movieList: [],
         tagList: [],
+        allTagList: [],
+        allTagTagsList: [],
+        allMovieList: [],
         isShow: false,
     },
     mounted() {
@@ -16,11 +19,22 @@ const app = new Vue({
             .then(res => {
                 console.log(res.data);
                 this.tagList = res.data;
+                this.allTagList = res.data;
             });
         axios.get(`movie/tag/0`)
             .then(res => {
                 console.log(res.data);
-                this.movieList = res.data;
+                // this.movieList = res.data[0]['movies'];
+                this.allMovieList = res.data;
+                this.movieList = res.data.filter(function (item, index) {
+                    if (item.tag == "all") return true;
+                })
+                this.movieList = this.movieList[0].movies
+            });
+        axios.get(`movie/tag_tag/0`)
+            .then(res => {
+                console.log(res.data);
+                this.allTagTagsList = res.data;
             });
     },
     methods: {
@@ -42,31 +56,30 @@ const app = new Vue({
                 }
             }
         },
+        sortAllTagList(tag) {
+            var topTags = this.allTagTagsList.filter(function (item, index) {
+                if (item.tag == tag) return true;
+            })[0].tags
+
+        },
         resetTag() {
             this.pushedTags = [];
-        }
+        },
     },
     watch: {
         pushedTags: function (val) {
             axios.get(`movie/tag/${val}`)
                 .then(res => {
                     console.log(res.data);
-                    this.movieList = res.data;
+                    this.movieList = res.data[0].movies;
                 });
             if (val == '') {
-                axios.get(`movie/movie_tag/0`)
-                    .then(res => {
-                        console.log(res.data);
-                        this.tagList = res.data;
-                    });
-                axios.get(`movie/tag/0`)
-                    .then(res => {
-                        console.log(res.data);
-                        this.movieList = res.data;
-                    });
+                this.tagList = this.allTagList;
+                this.movieList = this.allMovieList[0].movies;
             }
         },
         movieList: function (val) {
+            // var movie_ids = [];
             var movie_ids = [];
             for (let v of val) {
                 movie_ids.push(v.id);
@@ -83,7 +96,24 @@ const app = new Vue({
                     this.tagList.splice(i, 1);
                 }
             }
-        }
+        },
+    },
+    computed: {
+        tagsMovieList: function () {
+            return function (tag) {
+                return this.allMovieList.filter(function (item, index) {
+                    if (item.tag == tag) return true;
+                })[0].movies
+            }
+        },
+        
+        tagTagsList: function () {
+            return function (tag) {
+                return this.allTagTagsList.filter(function (item, index) {
+                    if (item.tag == tag) return true;
+                })[0].tags
+            }
+        },
     },
 
  })
